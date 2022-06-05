e17202b00c6cfe320e24d2e3b487ad1eb265fec6
samani21 <eksloba21@gmail.com>
2022-06-05 23:00:41 +0800

    Menampilkan Item Secara Dinamis

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,14 @@ class MyApp extends StatelessWidget {
 }
 
 class FirstScreen extends StatelessWidget {
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: <Widget>[
-          Container(
+        children: numberList.map((number) {
+          return Container(
             height: 250,
             decoration: BoxDecoration(
               color: Colors.grey,
@@ -32,51 +34,12 @@ class FirstScreen extends StatelessWidget {
             ),
             child: Center(
               child: Text(
-                '1',
+                '$number', // Ditampilkan sesuai item
                 style: TextStyle(fontSize: 50),
               ),
             ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: Center(
-              child: Text(
-                '2',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: Center(
-              child: Text(
-                '3',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
-            ),
-            child: Center(
-              child: Text(
-                '4',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-        ],
+          );
+        }).toList(),
       ),
     );
   }

6d0086bab539b2f3fde0f84419c01f1e00b654bb
samani21 <eksloba21@gmail.com>
2022-06-05 22:19:01 +0800

    membuat radio button

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
-  bool? agree = false;
+  String? language;
 
   @override
   Widget build(BuildContext context) {
@@ -40,16 +40,58 @@ class _FirstScreenState extends State<FirstScreen> {
       appBar: AppBar(
         title: Text('First Screen'),
       ),
-      body: ListTile(
-        leading: Checkbox(
-          value: agree,
-          onChanged: (bool? value) {
-            setState(() {
-              agree = value;
-            });
-          },
-        ),
-        title: Text('Agree / Disagree'),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: Radio<String>(
+              value: 'Dart',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Dart'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: 'Kotlin',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Kotlin'),
+          ),
+          ListTile(
+            leading: Radio<String>(
+              value: 'Swift',
+              groupValue: language,
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                  showSnackbar();
+                });
+              },
+            ),
+            title: Text('Swift'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void showSnackbar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$language selected'),
+        duration: Duration(seconds: 1),
       ),
     );
   }

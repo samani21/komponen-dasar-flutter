8142788211088915102ee407aef844c3916db703
samani21 <eksloba21@gmail.com>
2022-06-05 21:49:43 +0800

    Widget Row merapat ditengah

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class FirstScreen extends StatelessWidget {
         title: Text('First Screen'),
       ),
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Icon(Icons.share),
           Icon(Icons.thumb_up),

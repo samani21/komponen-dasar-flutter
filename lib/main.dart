f3b86625ebf444dd18c27444594d577a3fffb272
samani21 <eksloba21@gmail.com>
2022-06-05 21:06:12 +0800

    membuat halaman kosong pada Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,57 +13,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Center(
-          child: BiggerText(
-              text: "Hello world!"), // Ubah widget Heading ke PerubahanText
-        ),
-      ),
+      home: FirstScreen(), // Panggil FirstScreen di sini
     );
   }
 }
 
-class Heading extends StatelessWidget {
-  final String text;
-
-  Heading({required this.text});
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({required this.text});
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
-    );
+    return Scaffold();
   }
 }
